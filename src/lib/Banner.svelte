b587303a3d1f10b2c5c2c1cced2df50ccc980bbb
Etienne <eti@naude.dev>
2022-10-11 00:20:42 +1300

    banner comp

---
+++ src/lib/Banner.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let bannerInfo = {
+		style: '',
+		text: '',
+		display: false
+	};
+</script>
+
+{#if bannerInfo.display}
+	<div class="banner {bannerInfo.style}">{bannerInfo.text}</div>
+{/if}
